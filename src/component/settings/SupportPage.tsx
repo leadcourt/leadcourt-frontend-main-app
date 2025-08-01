a5c16d8e11eff2164bccec06af6c6e1571979a71
ladisamuel <yyemiteamazing@gmail.com>
2025-08-01 13:28:57 -0700

    fix & feats:

--- src/component/settings/SupportPage.tsx
+++ src/component/settings/SupportPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ChevronRight, FileText, Mail } from "lucide-react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 interface SupportOptionProps {
   icon: React.ReactNode;
